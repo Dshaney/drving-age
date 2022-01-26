e1d7ba4bd0d20eadfa3ba65cab0148ba21b8298e
Dshane Godbold <dshanegodbold@gmail.com>
2022-01-26 11:47:32 -0600

    u[date ]

--- main.js
+++ main.js
@@ -2,14 +2,18 @@ import input from 'input';
 
 async function main() {
     const drivingAge = 18;
-    const myAge = 28;
+    const myAge = 29;
     const hasLicense = await input.confirm('Do you happen to have a License?');
 
     if (hasLicense&& myAge>=drivingAge) {
         console.log('You can Drive')
-    }
-    else {
+    }else {
         console.log('You cannot Drive')
+        if (myAge < drivingAge) {
+            console.log(`Wait ${drivingAge - myAge} Years`)        
+        }else { 
+            console.log('go get you License!')
+        }
     }
 }
 main ()
